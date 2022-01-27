29eeaa3b6ea27f918a1fb9857501f76ccb8d50f4
Werner Liechti <werner@saynode.ch>
2022-01-27 13:46:43 +0100

    Init

---
+++ DHN.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "./ERC20.sol";
+
+
+contract DHN is ERC20() {
+  /**
+  * @param wallet Address of the wallet, where tokens will be transferred to
+  */
+  constructor(address wallet)  {
+    require(wallet != address(0),'No wallet was provided');
+    _mint(wallet, 372000000 ether);
+  }
+}
